a21b4c15f91514e11334da61472334d71c342cf3
sunnywp <wang.ping6@zte.com.cn>
2020-05-23 15:10:55 +0800

    fix retriePassword

--- src/server.js
+++ src/server.js
@@ -159,10 +159,10 @@ app.put('/api/v1.0/iam/users/:id/pwd', function (req, res) {
 });
 
 app.put('/api/v1.0/iam/retrievePwd', function (req, res) {
-    const id = req.params['id']
     if (!req.body) return res.sendStatus(400);
-    User.findOne({'name': req.body['name']}).then((person) => {
-            User.updateOne({"usrId": person.usrId}, {"password": req.body.password}).then(() => {
+    console.log(req.body);
+    User.findOne({'name': req.body.name}).then((person) => {
+            User.updateOne({"name": req.body.name}, {"password": req.body.password}).then(() => {
                 res.status(200).json({});
             }, () => {
                 res.status(400).json({"error": "找回密码失败，数据库异常"});
